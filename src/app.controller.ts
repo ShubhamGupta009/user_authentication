bc18a7fa09c73abd0285cced032f19efbd5fb8e3
ShubhamGupta009 <108325274+ShubhamGupta009@users.noreply.github.com>
2024-05-15 15:32:27 +0530

    initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -17,7 +18,13 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req)
   {
-    //return this.authService.login(req.user);
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req)
+  {
     return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
     constructor(private usersService: UsersService, private jwtService: JwtService ) { }
     async validateUser(username: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(username);
-        console.log(user);
+        //console.log(user);
         if(user && user.password === pass)
         {
             const { password, ...result } = user
@@ -23,9 +23,9 @@ export class AuthService {
     async login(user: any)
     {
         const payload = { username: user.username, sub: user.userId };
-        return 
-        { 
-            access_token: this.jwtService.sign(payload)
-        };
+        //console.log(payload);
+        const token = this.jwtService.sign(payload);
+        // // Return the token to the client
+        return { accessToken: token };
     }
 }
